822229c884bebbb479e039c0db75e077296bad46
Ebtesam Shemis <82912529+Ebtesam123@users.noreply.github.com>
2021-08-22 00:38:27 +0200

    Update Local_Api.js

--- src/utils/Local_Api.js
+++ src/utils/Local_Api.js
@@ -5,7 +5,7 @@ import {
   _saveQuestionAnswer,
 } from "./_DATA";
 
-export function getInitialData() {
+export function GetUsersQuestion() {
   return Promise.all([_getUsers(), _getQuestions()]).then(
     ([users, questions]) => ({
       users,
@@ -18,6 +18,6 @@ export function saveQuestion(question) {
   return _saveQuestion(question);
 }
 
-export function saveQuestionAnswer(authUser, qid, answer) {
-  return _saveQuestionAnswer({ authUser, qid, answer });
+export function saveQuestionAnswer(LoggedUser, qid, answer) {
+  return _saveQuestionAnswer({ LoggedUser, qid, answer });
 }
