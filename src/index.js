92e22fd5d6e3d334a06c098ca4fbcdb139cb6d55
Ebtesam Shemis <82912529+Ebtesam123@users.noreply.github.com>
2021-08-22 00:52:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import App from "./components/App";
 import "./index.css";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./reducers/index";
+import rootReducer from "./reducers/ComineReducers.js";
 import thunk from "redux-thunk";
 import { applyMiddleware } from "redux";
 
 const Log = (store) => (nxt) => (act) => {
+  console.group(act.type);
   console.log("Action:", act);
-  return nxt(act);
+  const result = nxt(act);
+  console.log("New State: ", store.getState());
+  console.groupEnd();
+  return result;
 };
 
 const store = createStore(rootReducer, applyMiddleware(thunk, Log));
