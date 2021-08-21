2f01ad610d48bae3ba6d531e778f678c24fb9c76
Ebtesam Shemis <82912529+Ebtesam123@users.noreply.github.com>
2021-08-22 00:18:45 +0200

    Update authUser.js

--- src/actions/authUser.js
+++ src/actions/authUser.js
@@ -1,8 +1,8 @@
-import { SET_AUTH_USER } from "./actionTypes";
+import { SET_AUTHORIZED_USER } from "./actionTypes";
 
 export function setAuthUser(id) {
   return {
-    type: SET_AUTH_USER,
+    type: SET_AUTHORIZED_USER,
     id,
   };
 }
